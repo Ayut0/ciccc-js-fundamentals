45f9114393af9473826ec4b931721dcb825fb1a1
Yuto Yamakita <yuvto.0420@gmail.com>
2022-05-09 16:13:25 -0700

    Give an answers

--- exercises/ex28.js
+++ exercises/ex28.js
@@ -11,3 +11,30 @@ deleteMessage(index) - removes the message corresponding to the index provided
 addFriend() - increases the friend count by 1
 removeFriend() - decreases the friend count by 1
 */
+
+const facebookProfile = {
+    name: "Yuto",
+    friends : 300,
+    messages : ["Summer is around the corner", "I wish I could stop getting caffeine", "I wanna visit U.S. again"],
+
+    post: function postMessage(message){
+        this.messages.push(message);
+    },
+
+    delete: function deleteMessage(index){
+        this.messages.splice(index, 1);
+    },
+
+    add: function addFriend(){
+        this.friends++;
+    },
+
+    remove: function removeFriend(){
+        this.friends--;
+    }
+}
+
+facebookProfile.post("aaa");
+facebookProfile.delete(3);
+facebookProfile.remove();
+console.log(facebookProfile);
